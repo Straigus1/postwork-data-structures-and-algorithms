af10dd017fc4e37ccc2560037d504b255717fd89
Dayton <kirylmitch@gmail.com>
2022-04-06 14:42:13 -0400

    solved day 3 problem

--- 01-week-1--starter-algorithms/02-day-3--fibonacci-series/javascript/fibonacci.js
+++ 01-week-1--starter-algorithms/02-day-3--fibonacci-series/javascript/fibonacci.js
@@ -1,12 +1,19 @@
 function fibonacci(num) {
   // type your code here
+  let numContainer = [0, 1]
+  let currentNum = 0
+  for (let i = 2; i <= num; i++) {
+    currentNum = numContainer[i-2] + numContainer[i-1]
+    numContainer.push(currentNum)
+  }
+  return numContainer[num]
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 0");
   console.log("=>", fibonacci(0));
-
+  console.log(fibonacci(20))
   console.log("");
 
   console.log("Expecting: 1");
@@ -20,5 +27,6 @@ if (require.main === module) {
 
 module.exports = fibonacci;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+// Create a fibonacci sequence within the function. Make an Array that starts with 0 and 1, then add the values together over and ove based on number(param)
+
+// Created a fibonacci sequence that looped based on the number(arguement). Then called the index from ym new array.

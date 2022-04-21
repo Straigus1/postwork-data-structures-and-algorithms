61f99c2f841c5c22e986f3b6a3e13574b50c7038
Dayton <kirylmitch@gmail.com>
2022-04-21 15:23:59 -0400

    solved bonus 02

--- 03-week-3--additional-practice/01-bonus-2--roman-numeral-to-integer/javascript/roman_numeral.js
+++ 03-week-3--additional-practice/01-bonus-2--roman-numeral-to-integer/javascript/roman_numeral.js
@@ -1,5 +1,72 @@
 function romanNumeral(string) {
-  // type your code here
+  // Convert characters in a string into numerical values
+  // If lower letter is before bigger number, subtract that value
+  let num = 0
+  for (let i = 0; i < string.length; i++) {
+    if (string.charAt(i) == "I") {
+      num = num + 1
+    }
+    if (string.charAt(i) == "V") {
+      num = num + 5
+    }
+    if (string.charAt(i-1) == "I" && (string.charAt(i) == "V" || string.charAt(i) == "X")) {
+      num = num - 2
+    }
+    if (string.charAt(i) == "X") {
+      num = num + 10
+    }
+    if (string.charAt(i) == "L") {
+      num = num + 50
+    }
+    if (string.charAt(i-1) == "X" && (string.charAt(i) == "L" || string.charAt(i) == "C")) {
+      num = num - 20
+    }
+    if (string.charAt(i) == "C") {
+      num = num + 100
+    }
+    if (string.charAt(i) == "D") {
+      num = num + 500
+    }
+    if (string.charAt(i-1) == "C" && (string.charAt(i) == "D" || string.charAt(i) == "M")) {
+      num = num - 200
+    }
+    if (string.charAt(i) == "M") {
+      num = num + 1000
+    }
+   
+  }
+  return num
+  // const romans = {
+  //   I: 1,
+  //   IV: 4,
+  //   V: 5,
+  //   IX: 9,
+  //   X: 10,
+  //   XL: 40,
+  //   L: 50,
+  //   XC: 90,
+  //   C: 100,
+  //   CD: 400,
+  //   D: 500,
+  //   CM: 900,
+  //   M: 1000
+  // };
+  // let total = 0;
+  // let idx = 0;
+
+  // while (idx < string.length) {
+  //   const twoChar = string[idx] + (string[idx + 1] || '');
+
+  //   if (romans[twoChar] !== undefined) {
+  //     total += romans[twoChar];
+  //     idx += 2;
+  //   } else {
+  //     total += romans[string[idx]];
+  //     ++idx;
+  //   }
+  // }
+
+  // return total;
 }
 
 if (require.main === module) {
